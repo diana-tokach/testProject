96cad6955d16c28cdd59fb8f5be5bbcc1c2c8436
diana-tokach <m.diana.tokach@gmail.com>
2020-05-10 01:09:22 +0300

    some text

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+var number = 1;
+var string = 'Tokach';
+var sym = Symbol();
+var boolean = true;
+var nullVar = null;
+var undefinedVar = undefined;
+var obj = {};
+
+let ratata = ['test str 0', 'test str 1', 'test str 2'];
+
+console.log(ratata[0]);
